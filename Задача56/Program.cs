5c6d43d0b0df4dd5cfe3c437092a7ac57e045e36
Ergo322 <Shikmen2@mail.ru>
2022-09-19 18:30:43 +0300

    task56

---
+++ Задача56/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+void CreationArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumNum(int[,] array, int i)
+{
+    int sumLine = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sumLine += array[i, j];
+    }
+    return sumLine;
+}
+
+void SearchSum()
+{
+    int MinSumLine = 0;
+    int sumLine = SumNum(array, 0);
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        int tempSumLine = SumNum(array, i);
+        if (sumLine > tempSumLine)
+        {
+            sumLine = tempSumLine;
+            MinSumLine = i;
+        }
+    }
+    Console.WriteLine($"\n{MinSumLine + 1} - строка с наименшей суммой");
+}
+
+CreationArray(array);
+Console.WriteLine();
+PrintArray(array);
+SearchSum();
