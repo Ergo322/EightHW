9bb9d25ee80d602e81604bc1f50dd13bd7ad8813
Ergo322 <Shikmen2@mail.ru>
2022-09-19 18:29:33 +0300

    task54

---
+++ Задача54/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+
+void CreationArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PorydokArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int g = 0; g < array.GetLength(1) - 1; g++)
+            {
+                if (array[i, g] < array[i, g + 1])
+                {
+                    int temp = array[i, g + 1];
+                    array[i, g + 1] = array[i, g];
+                    array[i, g] = temp;
+                }
+            }
+        }
+    }
+}
+
+CreationArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Отсортированый массив");
+Console.WriteLine();
+PorydokArray(array);
+PrintArray(array);
